6f03d4115280ee95f1b34c2ca11519b05da9b186
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-08-03 12:22:43 +0200

    1.1

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const listOfCategories = document.getElementById('categories');
+const categoriesEl = listOfCategories.querySelectorAll('li.item');
+const numberOfCategories = categoriesEl.length;
+console.log('Number of categories:', numberOfCategories)
