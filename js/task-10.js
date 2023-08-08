d16e3775765fe97cb05d5e3e2ebb659585c2c59b
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-08-08 15:25:53 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector('#boxes');
+  boxesContainer.innerHTML = ''; 
+  
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    
+    boxesContainer.appendChild(box);
+    
+    size += 10;
+  }
+}
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+
+createButton.addEventListener('click', onCreateBoxes);
+destroyButton.addEventListener('click', onDestroyBoxes);
+
+function onCreateBoxes(){
+  const amount = parseInt(document.querySelector('input').value);
+  createBoxes(amount);
+}
+
+function onDestroyBoxes() {
+  const boxesContainer = document.querySelector('#boxes');
+  boxesContainer.innerHTML = ''; 
+}
