5df852be951ad512c3120f801e049bc0fd6de04c
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-08-10 09:56:35 +0200

    fix

--- js/task-09.js
+++ js/task-09.js
@@ -3,8 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, '0')}`;
 }
-const widgetContent = document.querySelector('.widget');
+const bodyColor = document.querySelector('body');
 const buttonChangeColor = document.querySelector('.change-color');
-
+const colorName = document.querySelector('.color');
 buttonChangeColor.addEventListener('click', onChangeColor);
-function onChangeColor(event) {widgetContent.style.backgroundColor = getRandomHexColor()};
+function onChangeColor(event) {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = bodyColor.style.backgroundColor;
+};
